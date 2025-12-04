70b22c3e36950e07d68af489036447e3fd55d409
Charlie Allison <76861693+csa5418@users.noreply.github.com>
2025-12-04 13:14:55 -0500

    Now with validation :)

--- ML.py
+++ ML.py
@@ -19,7 +19,7 @@ emotions = pd.read_csv("C:\\Users\\csa54\\OneDrive\\Documents\\CSCI_4366\\datase
 training = images[:2500]
 testing= images[2500:]
 trainingcsv = emotions[:2500]
-testingcsv = emotions[2500: 300]
+testingcsv = emotions[2500:]
 #print(len(training))
 #print(training[2499])
 #print(trainingcsv.head())
@@ -27,30 +27,25 @@ testingcsv = emotions[2500: 300]
 #grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #newimg = grey.reshape(1500, 1980, 1).astype('float32')
 
-
 x_train = []
 
 #print(newimg.shape)
 for i in range(0, 2500):
     img = cv2.imread("C:\\Users\\csa54\\OneDrive\\Documents\\CSCI_4366\\dataset\\TrainImageFiles"+"\\"+training[i])
-    img = cv2.resize(img, (32, 32))
+    img = cv2.resize(img, (64, 64))
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     #grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     #print(len(grey))
     #newimg = grey.reshape(1500, 1980, 3).astype('float32')
     x_train.append(img)
 
-''''
-print(np.array(x_train).shape)
 
 x_test = []
-for i in range(0, 10):
+for i in range(0, 500):
     img = cv2.imread("C:\\Users\\csa54\\OneDrive\\Documents\\CSCI_4366\\dataset\\TrainImageFiles"+"\\"+testing[i])
-    grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #print(type(grey))
-    newimg = grey.reshape(1500, 1980, 1).astype('float32')
-    x_test.append(newimg)
-'''
+    img = cv2.resize(img, (64, 64))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    x_test.append(img)
 
 #print(img.shape)
 #print(img)
@@ -66,9 +61,15 @@ y_train_new = []
 for i in range(0, y_train.size):
     start = np.array([y_train[i]])
     y_train_new.append(start)
+
+y_test_new = []
+#print(y_test.head)
+for i in range(2501, 3001):
+    start = np.array([y_test[i]])
+    y_test_new.append(start)
 #plt.imshow(grey, cmap='gray')
 #print(img.shape)
-#print(x_train)
+#print(len(y_test_new))
 #plt.show()
 print(np.array(x_train).shape)
 print(np.array(y_train_new).shape)
@@ -76,17 +77,20 @@ print(np.array(y_train_new).shape)
 model = Sequential()
 
 # Convolutional Phase
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape= (32, 32, 3)))  
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape= (64, 64, 3)))  
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
 
 model.add(Flatten())
 
-model.add(Dense(64, activation='relu'))
+model.add(Dense(128, activation='relu'))
 model.add(Dense(7, activation='softmax'))
 
 model.compile(loss="sparse_categorical_crossentropy", optimizer='adam', metrics=['accuracy'])
-model.fit(np.array(x_train), np.array(y_train_new), epochs=3)
-model.summary()
+model.fit(np.array(x_train), np.array(y_train_new), epochs=10)
+score, accurary = model.evaluate(np.array(x_test), np.array(y_test_new))
+print(score)
+print(accurary)
+#model.summary()
